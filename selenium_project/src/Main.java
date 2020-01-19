eb15e323a8b8c5161f7ee01490f98ac282b6d21f
Larysa Shmorgun <lshmor@softserveinc.com>
2020-01-19 18:49:51 +0200

    added selenium project to my repository

---
+++ selenium_project/src/Main.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myUrl = "www.leafground.com/pages/Button.html";
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		ChromeDriver driver = new ChromeDriver(options);
+		//driver.manage().window().maximize();
+		driver.navigate().to("http://www.leafground.com/pages/Window.html");
+		String mainWindow = driver.getWindowHandle();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("color")).click();
+		driver.navigate().to("http://www.leafground.com/pages/Button.html");
+		String secondWindow = driver.getWindowHandle();
+		System.out.println(driver.getTitle());
+		driver.switchTo().window(mainWindow);
+	
+	}
+}
